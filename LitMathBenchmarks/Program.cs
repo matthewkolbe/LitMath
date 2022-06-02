eca2f738e38493ee45ee96d5634699501a2ad2e9
matthewkolbe <matthew.kolbe@gmail.com>
2022-06-02 15:24:24 -0600

    Adds benchmarks to runner

--- LitMathBenchmarks/Program.cs
+++ LitMathBenchmarks/Program.cs
@@ -13,7 +13,8 @@ BenchmarkRunner.Run(new[]{
             BenchmarkConverter.TypeToBenchmarks( typeof(ExpBenchmark), config),
             BenchmarkConverter.TypeToBenchmarks( typeof(LogBenchmark), config),
             BenchmarkConverter.TypeToBenchmarks( typeof(TrigBenchmark), config),
-            BenchmarkConverter.TypeToBenchmarks( typeof(DotProductBenchmark), config)
+            BenchmarkConverter.TypeToBenchmarks( typeof(DotProductBenchmark), config),
+            BenchmarkConverter.TypeToBenchmarks( typeof(PolynomialBenchmark), config)
             });
 
 Console.ReadLine();
