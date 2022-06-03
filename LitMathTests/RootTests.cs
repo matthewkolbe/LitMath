dbbeefd6b639d4b1b4f8b005a8a05a1731d4f1a7
matthewkolbe <matthew.kolbe@gmail.com>
2022-06-02 21:57:36 -0600

    Added Sqrt float test

--- LitMathTests/RootTests.cs
+++ LitMathTests/RootTests.cs
@@ -22,5 +22,25 @@ namespace LitMathTests
                     Assert.AreEqual(1.0, Math.Sqrt(a[i]) / b[i], 1e-10);
             }
         }
+
+        [Test]
+        public void SqrtFloatAccuracySpan()
+        {
+            foreach (var n in new[] { 1, 3, 9, 15, 33, 62, 255, 1000 })
+            {
+                Span<float> a = stackalloc float[n];
+                Span<float> b = stackalloc float[n];
+                var r = new Random();
+
+                for (int i = 0; i < n; ++i)
+                    a[i] = (float)((i + 1) + r.NextDouble());
+
+
+                LitRoot.Sqrt(ref a, ref b);
+
+                for (int i = 0; i < n; ++i)
+                    Assert.AreEqual(1.0, Math.Sqrt(a[i]) / b[i], 1e-7);
+            }
+        }
     }
 }
