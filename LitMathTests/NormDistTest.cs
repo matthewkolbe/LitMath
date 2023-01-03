a786eb40386349930289d8c90f9f4b7efc49f444
matthewkolbe <matthew.kolbe@gmail.com>
2023-01-02 23:34:15 -0700

    Update NormDistTest.cs

--- LitMathTests/NormDistTest.cs
+++ LitMathTests/NormDistTest.cs
@@ -11,62 +11,68 @@ namespace LitMathTests
         [Test]
         public void ErfDoubleAccuracy()
         {
-            var n = 1000;
-            Span<double> x = new double[n];
-            Span<double> y = new double[n];
-            var r = new Random(10);
+            foreach (var n in new[] { 1, 3, 9, 15, 33, 62, 1000 })
+            {
+                Span<double> x = new double[n];
+                Span<double> y = new double[n];
+                var r = new Random(10);
 
-            for (int i = 0; i < n; i++)
-                x[i] = 30.0*(r.NextDouble()-0.5);
+                for (int i = 0; i < n; i++)
+                    x[i] = 30.0 * (r.NextDouble() - 0.5);
 
-            Lit.Erf(in x, ref y);
+                Lit.Erf(in x, ref y);
 
-            for (int i = 0; i < n; ++i)
-                Assert.AreEqual(y[i], SpecialFunctions.Erf(x[i]), 1e-13);
+                for (int i = 0; i < n; ++i)
+                    Assert.AreEqual(y[i], SpecialFunctions.Erf(x[i]), 1e-13);
+            }
         }
 
         [Test]
         public void CdfDoubleAccuracy()
         {
-            var n = 1000;
-            Span<double> x = new double[n];
-            Span<double> y = new double[n];
-            Span<double> m = new double[n];
-            Span<double> s = new double[n];
-
-            for (int i = 0; i < n; i++)
+            foreach (var n in new[] { 1, 3, 9, 15, 33, 62, 1000 })
             {
-                x[i] = 10 * (-0.5 * n + i) / n;
-                m[i] = (-0.05 * (n + i) / n);
-                s[i] = (i % 10) + 0.5;
+                Span<double> x = new double[n];
+                Span<double> y = new double[n];
+                Span<double> m = new double[n];
+                Span<double> s = new double[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    x[i] = 10 * (-0.5 * n + i) / n;
+                    m[i] = (-0.05 * (n + i) / n);
+                    s[i] = (i % 10) + 0.5;
+                }
+
+                Lit.CDF(in m, in s, in x, ref y);
+
+                for (int i = 0; i < n; ++i)
+                    Assert.AreEqual(y[i], Normal.CDF(m[i], s[i], x[i]), 1e-13);
             }
-
-            Lit.CDF(in m, in s, in x, ref y);
-
-            for (int i = 0; i < n; ++i)
-                Assert.AreEqual(y[i], Normal.CDF(m[i], s[i], x[i]), 1e-13);
         }
 
         [Test]
         public void CdfDoubleStandardAccuracyBig()
         {
-            var n = 1000;
-            Span<double> x = new double[n];
-            Span<double> y = new double[n];
-            var really = new double[n];
-            var m = 0.0;
-            var s = 1.0;
-
-            for (int i = 0; i < n; i++)
+            foreach (var n in new[] { 1, 3, 9, 15, 33, 62, 1000 })
             {
-                x[i] = 20 * (-0.5 * n + i) / n;
-                really[i] = Normal.CDF(m, s, x[i]);
+                Span<double> x = new double[n];
+                Span<double> y = new double[n];
+                var really = new double[n];
+                var m = 0.0;
+                var s = 1.0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    x[i] = 20 * (-0.5 * n + i) / n;
+                    really[i] = Normal.CDF(m, s, x[i]);
+                }
+
+                Lit.CDF(m, s, in x, ref y);
+
+                for (int i = 0; i < n; ++i)
+                    Assert.AreEqual(y[i], really[i], 1e-9);
             }
-
-            Lit.CDF(m, s, in x, ref y);
-
-            for (int i = 0; i < n; ++i)
-                Assert.AreEqual(y[i], really[i], 1e-9);
         }
 
         [Test]
@@ -117,45 +123,49 @@ namespace LitMathTests
         [Test]
         public void CdfFloatAccuracy()
         {
-            var n = 1000;
-            Span<double> x = new double[n];
-            Span<double> y = new double[n];
-            Span<double> m = new double[n];
-            Span<double> s = new double[n];
-
-            for (int i = 0; i < n; i++)
+            foreach (var n in new[] { 1, 3, 9, 15, 33, 62, 1000 })
             {
-                x[i] = 10f * (-0.5f * n + i) / n;
-                m[i] = (-0.05f * (n + i) / n);
-                s[i] = (i % 10) + 0.5f;
+                Span<double> x = new double[n];
+                Span<double> y = new double[n];
+                Span<double> m = new double[n];
+                Span<double> s = new double[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    x[i] = 10f * (-0.5f * n + i) / n;
+                    m[i] = (-0.05f * (n + i) / n);
+                    s[i] = (i % 10) + 0.5f;
+                }
+
+                Lit.CDF(in m, in s, in x, ref y);
+
+                for (int i = 0; i < n; ++i)
+                    Assert.AreEqual(y[i], Normal.CDF(m[i], s[i], x[i]), 2e-7);
             }
-
-            Lit.CDF(in m, in s, in x, ref y);
-
-            for (int i = 0; i < n; ++i)
-                Assert.AreEqual(y[i], Normal.CDF(m[i], s[i], x[i]), 2e-7);
         }
 
         [Test]
         public void CdffloatNonStandardAccuracy()
         {
-            var n = 1000;
-            Span<float> x = new float[n];
-            Span<float> y = new float[n];
-            var realy = new float[n];
-            var m = 0.2f;
-            var s = 1.03f;
-
-            for (int i = 0; i < n; i++)
+            foreach (var n in new[] { 1, 3, 9, 15, 33, 62, 1000 })
             {
-                x[i] = 30f * (-0.5f * n + i) / n;
-                realy[i] = (float)Normal.CDF((double)m, (double)s, (double)x[i]);
+                Span<float> x = new float[n];
+                Span<float> y = new float[n];
+                var realy = new float[n];
+                var m = 0.2f;
+                var s = 1.03f;
+
+                for (int i = 0; i < n; i++)
+                {
+                    x[i] = 30f * (-0.5f * n + i) / n;
+                    realy[i] = (float)Normal.CDF((double)m, (double)s, (double)x[i]);
+                }
+
+                Lit.CDF(m, s, in x, ref y);
+
+                for (int i = 0; i < n; ++i)
+                    Assert.AreEqual(y[i], realy[i], 1e-5);
             }
-
-            Lit.CDF(m, s, in x, ref y);
-
-            for (int i = 0; i < n; ++i)
-                Assert.AreEqual(y[i], realy[i], 1e-5);
         }
     }
 }
