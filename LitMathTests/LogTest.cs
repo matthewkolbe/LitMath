b5b1724a1f2ce5f609d7e071c71b7fdf4400c10e
matthewkolbe <matthew.kolbe@gmail.com>
2023-11-15 17:43:22 -0700

    Update LogTest.cs

--- LitMathTests/LogTest.cs
+++ LitMathTests/LogTest.cs
@@ -21,7 +21,7 @@ namespace LitMathTests
                 Lit.Ln(in a, ref b);
 
                 for (int i = 0; i < n; ++i)
-                    Assert.AreEqual(1.0, Math.Log(a[i]) / b[i], 1e-8);
+                    Assert.AreEqual(1.0, Math.Log(a[i]) / b[i], 5e-14);
             }
         }
 
@@ -103,6 +103,29 @@ namespace LitMathTests
 
             Assert.AreEqual(b[0], float.PositiveInfinity);
             Assert.IsTrue(float.IsNaN(b[1]));
+        }
+
+        [Test]
+        public void AvxLogApprxDoubleAccuracy()
+        {
+            int n = 4;
+            Span<double> a = stackalloc double[n];
+            Span<double> b = stackalloc double[n];
+            var r = new Random(10);
+
+            for (int i = 0; i < n; ++i)
+                a[i] = 1 + r.NextDouble();
+
+            var aa = Util.LoadV256(a[0], 0);
+            var bb = Util.LoadV256(b[0], 0);
+
+            Lit.LogApprox(in aa, ref bb);
+
+            Util.StoreV256(ref b[0], 0, bb);
+
+            for (int i = 0; i < n; ++i)
+                Assert.AreEqual(b[i], Math.Log(a[i]), 1e-14);
+            
         }
     }
 }
